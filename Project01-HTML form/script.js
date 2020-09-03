6cc8d33f094d42b799c5cc5c333831be82cc7548
KhawajaSaad <kh.saad7223@gmail.com>
2020-09-03 23:29:52 +0500

    JS file updates
    worked on function.

--- Project01-HTML form/script.js
+++ Project01-HTML form/script.js
@@ -4,4 +4,51 @@ const email = document.getElementById('email');
 const password = document.getElementById('password');
 const password2 = document.getElementById('password2');
 
-form.addEventListener('submit',function(){})
+// All Functions
+// Function to show error.
+function showError(input,message) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error'; //test += 'error' later.
+    const small = formControl.querySelector('small');
+    small.innerText = message;
+
+}
+
+// Function to show success
+function showSuccess(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
+
+
+}
+
+
+
+// This is an event listener for the form on submit.
+form.addEventListener('submit',function(e) {
+    e.preventDefault();
+    if(username.value === '') {
+        showError(username,'Username is required');
+    }else {
+        showSuccess(username);
+    }
+    // checking email.
+    if(email.value === '') {
+        showError(email,'email is required');
+    }else {
+        showSuccess(email);
+    }
+    // checking password.
+    if(password.value === '') {
+        showError(password,'Password is required');
+    }else {
+        showSuccess(password);
+    }
+    // checking password2.
+    if(password2.value === '') {
+        showError(password2,'Confirm password is required');
+    }else {
+        showSuccess(password2);
+    }
+
+})
